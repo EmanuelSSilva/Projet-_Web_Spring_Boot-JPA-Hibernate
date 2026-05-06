74c02e08122bd4ddf1358be3f9061910cd554952
Emanuel Silva <emanuellinssilva65@gmail.com>
2026-05-06 17:29:53 -0300

    User entity e Resource

---
+++ src/main/java/com/projetoback/spring_jpa_hibernate/resourse/UserResource.java
@@ -0,0 +1,22 @@
+package com.projetoback.spring_jpa_hibernate.resourse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoback.spring_jpa_hibernate.entities.User;
+
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User (1L, "Emanuel", "emanuel@gmail.com.br", "99999999", "123456789");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
